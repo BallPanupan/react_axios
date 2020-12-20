cfd333bcc75ea3b8cd45d0c204c5b9505d87a78f
PanupanSekitori <Ball085573@hotmail.com>
2020-12-21 00:32:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
+const api = axios.create({
+  baseURL: 'http://jsonplaceholder.typicode.com/users'
+})
+
+
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: []
+    }
+
+    axios.get('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.data)
+    .then((data) => {
+      this.setState({ courses: data})
+      console.log("state_courses :",this.state.courses)
+    })
+
+  }
+
+
+    createPost = async () => {
+      var data = [
+        {
+          firstName: 'Panupan',
+          Email: 'Ball08573@hotmail.com'
+        },
+        {
+          firstName: 'Ball',
+          Email: 'Null'
+        }
+      ]
+
+      // axios.post('/', data)
+      //   .then(function (response) {
+      //     console.log(response);
+      //   })
+      console.log(data);
+    }
+
+
+  createCourse = async () => {
+    let res = await api.post('/',
+      {
+        name:"Panupan",
+        Email:"Ball08573@hotmail.com"
+      }
+    )
+    console.log(res);
+  }
+
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
+      <button onClick={this.createCourse}>createCourse</button>
+      <button onClick={this.createPost}>createPost</button>
+        <div>
+          <ul>
+            {this.state.courses.map(course =>(
+              <li key={course.id}>
+              Name: {course.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +89,10 @@ function App() {
       </header>
     </div>
   );
+  }
+
 }
+
+
 
 export default App;
